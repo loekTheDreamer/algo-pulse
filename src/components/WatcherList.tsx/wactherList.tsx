4c98088f7668284e89c1f22dff48d3b49984912b
Luke Carelsen <luke.carelsen@me.com>
2025-02-16 13:18:54 +0200

    feat: added watcher list

--- src/components/WatcherList.tsx/wactherList.tsx
+++ src/components/WatcherList.tsx/wactherList.tsx
@@ -9,23 +9,18 @@ import {
 } from '@ui-kitten/components';
 import {StyleSheet, Image} from 'react-native';
 import {formatWalletAddress} from '../../utils/formatters';
-
-interface IListItem {
-  title: string;
-  description: string;
-}
-
-const data = new Array(8).fill({
-  title: 'J25ABPMWJLJECPMGWWM42BAG6BGILFY3VL732IAKP2YPCLURIE26LV6XYE',
-  description: 'Description for Item',
-});
+import {useWatcherListStore} from '../../store/useWatcherListStore';
+import {AccountInfo} from '../../api/api';
 
 const TrashIcon = (props: IconProps): IconElement => (
   <Icon {...props} name="trash-2" width={16} height={16} />
 );
 
 const WatcherList = (): React.ReactElement => {
-  const renderItemAccessory = (props: IconProps): React.ReactElement => (
+  const {wactherList, addWatcherItem, removeWatcherItem, clearWatcherList} =
+    useWatcherListStore();
+
+  const renderItemAccessory = (item: AccountInfo): React.ReactElement => (
     // <Icon {...props} name="trash-outline" />
     // <Button size="tiny"  >FOLLOW</Button>
     <Button
@@ -33,12 +28,13 @@ const WatcherList = (): React.ReactElement => {
       appearance="ghost"
       status="danger"
       accessoryLeft={TrashIcon}
+      onPress={() => removeWatcherItem(item)}
     />
   );
 
-  const renderItemIcon = (props: IconProps): IconElement => (
+  const renderItemIcon = (item: AccountInfo): IconElement => (
     <Image
-      source={{uri: `https://robohash.org/${'luke'}?set=set1&bgset=bg1`}}
+      source={{uri: `https://robohash.org/${item.address}?set=set1&bgset=bg1`}}
       style={{width: 30, height: 30}}
     />
   );
@@ -47,18 +43,20 @@ const WatcherList = (): React.ReactElement => {
     item,
     index,
   }: {
-    item: IListItem;
+    item: AccountInfo;
     index: number;
   }): React.ReactElement => (
     <ListItem
-      title={formatWalletAddress(item.title)}
-      description={`${item.description} ${index + 1}`}
-      accessoryLeft={renderItemIcon}
-      accessoryRight={renderItemAccessory}
+      title={formatWalletAddress(item.address)}
+      description={item.amount}
+      accessoryLeft={() => renderItemIcon(item)}
+      accessoryRight={() => renderItemAccessory(item)}
     />
   );
 
-  return <List style={styles.container} data={data} renderItem={renderItem} />;
+  return (
+    <List style={styles.container} data={wactherList} renderItem={renderItem} />
+  );
 };
 
 const styles = StyleSheet.create({
