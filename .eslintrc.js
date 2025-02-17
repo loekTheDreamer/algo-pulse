e3fbbdfeb9ee2cf3db45f60a59d617afae6ec292
Luke Carelsen <luke.carelsen@me.com>
2025-02-18 00:12:12 +0200

    Revert "fix: unit tests"
    This reverts commit f6992602a1f55601e709f45e0a9bebc0c4333748.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,4 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  overrides: [
-    {
-      files: ['**/*.test.ts', '**/*.test.tsx', 'jest.setup.js'],
-      env: {
-        jest: true,
-      },
-    },
-  ],
 };
--- src/components/ui/watcherList/watcherList.tsx
+++ src/components/ui/watcherList/watcherList.tsx
@@ -68,7 +68,6 @@ const WatcherList = ({
     index: number;
   }): React.ReactElement => (
     <ListItem
-      testID="watcher-item"
       title={formatWalletAddress(item.address)}
       description={`${formatAlgoAmount(item.amount)} ALGO ≈ ${
         algoPrice
